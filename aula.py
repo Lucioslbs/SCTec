96c2bfa9d37d8e70e37caddcebbdb516652e2148
Luciano Batista <lucioslbs>
2026-02-15 18:20:28 -0300

    Meu primeiro commit configurado

---
+++ aula.py
@@ -0,0 +1,11 @@
+print("Ola Mundo");
+print("Bem Vindo a Aula de Python");
+print("Vamos aprendrer a programar em Python");
+print("Python é uma linguagem de programação fácil de aprender e poderosa");
+print("Vamos começar a aprender Python hoje mesmo!");
+print("Python é uma linguagem de programação de alto nível, interpretada e de propósito geral");
+print("                                                 #############                               ");
+print("                                                 #           #                               ");
+print("                                                 #   Python  #                               ");
+print("                                                 #           #                               ");
+
