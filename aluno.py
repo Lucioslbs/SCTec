d78dfe4a63d40f121f4ee85160556a97e15c2b4d
Luciano Batista <lucioslbs>
2026-02-17 20:37:07 -0300

    atualização

---
+++ aluno.py
@@ -0,0 +1,9 @@
+aluno = ['João', 'Maria', 'Pedro', 'Ana', 'Carlos', 'Fernanda', 'Lucas', 'Mariana', 'Rafael', 'Beatriz']
+print(aluno[0])  # Acessa o primeiro elemento da lista
+print(aluno[1])  # Acessa o segundo elemento da lista   
+print(aluno[2])  # Acessa o terceiro elemento da lista
+print(aluno[3])  # Acessa o quarto elemento da lista
+print(aluno[4])  # Acessa o quinto elemento da lista
+print(aluno[5])  # Acessa o sexto elemento da lista
+aluno.remove('Carlos')  # Remove o elemento 'Carlos' da lista
+print(aluno)  # Imprime a lista após a remoção do elemento 'Carlos'
