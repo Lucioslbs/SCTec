22452e00a78dc322d3fed3b6e951f37c610bee14
Luciano Batista <lucioslbs>
2026-02-17 20:28:01 -0300

    atualização

--- listaTelefonica.py
+++ listaTelefonica.py
@@ -1,10 +1,16 @@
-contato = ('Lucio', '(21) 98484-7695', 'lucio@email.com');
+contato = ('Lucio', '(21) 98484-7695', 'lucio@email.com')
 
-telefone2 = [];
-telefone2.append(contato);
-print(telefone2);
+telefone2 = []
+telefone2.append(contato)
+print(telefone2)
 
-telefone3 = [("Lucio", "(21) 98484-7695", "lucio@email.com"),("Maria", "(21) 98765-4321", "maria@email.com")];  
-print(telefone3);
+telefone3 = [("Lucio", "(21) 98484-7695", "lucio@email.com"), ("Maria", "(21) 98765-4321", "maria@email.com")]
+print(telefone3)
 
-print(telefone3[0][1]); # Acessa o telefone do primeiro contato 
+print(telefone3[0][1])  # Acessa o telefone do primeiro contato
+
+telefone3_dict = {contato[0]: {"phone": contato[1], "email": contato[2]} for contato in telefone3}
+print(telefone3_dict)  # Imprime o dicionário criado a partir da lista de tuplas
+
+telefone3_dict.pop("Lucio")  # Remove o contato "Lucio" do dicionário
+print(telefone3_dict)  # Imprime o dicionário após a remoção do contato "Lucio" 
