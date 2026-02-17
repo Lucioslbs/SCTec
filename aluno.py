d96dbe6e5897d86ff97aa80028009a795cf0d530
Luciano Batista <lucioslbs>
2026-02-17 20:42:44 -0300

    atualização

--- aluno.py
+++ aluno.py
@@ -7,3 +7,8 @@ print(aluno[4])  # Acessa o quinto elemento da lista
 print(aluno[5])  # Acessa o sexto elemento da lista
 aluno.remove('Carlos')  # Remove o elemento 'Carlos' da lista
 print(aluno)  # Imprime a lista após a remoção do elemento 'Carlos'
+
+# Dica #
+# "Em programação, a simplicidade e a clareza são fundamentais. 
+# Nunca se esqueça de que a manutenção do código é tão importante quanto a sua criação."
+# "Escreva código que seja fácil de entender e de modificar, para que outros possam trabalhar com ele no futuro."
